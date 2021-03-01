11f92a7ca919e03fbc3f79df2ddf0656801944b6
gaidongxu <gaidongxu@dataexa.com>
2021-03-01 19:28:56 +0800

    新增数组练习题

--- src/main/java/com/gai/interview/array/Problem.java
+++ src/main/java/com/gai/interview/array/Problem.java
@@ -1,5 +1,7 @@
 package com.gai.interview.array;
 
+import org.apache.spark.sql.sources.In;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -234,15 +236,63 @@ public class Problem {
         }
     }
 
+    /*
+    *   输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下 4X4 矩阵
+    *    1  2  3  4
+    *    5  6  7  8
+    *    9 10 11 12
+    *   13 14 15 16
+    *   则依次打印出数字 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
+    * */
+    public static List<Integer> problem7(int[][] array){
+        List<Integer> result = new ArrayList<>();
+        if(array == null || array.length <= 0 || array[0].length <= 0){
+            return result;
+        }
+        int leftTopX = 0;
+        int leftTopY = 0;
+        int rightBottomX = array.length-1;
+        int rightBottomY = array[0].length-1;
+
+        while (rightBottomX >= leftTopX
+                && rightBottomY >= leftTopY){
+            //从左向右
+            for(int col = leftTopY; col <= rightBottomY; col++){
+                result.add(array[leftTopX][col]);
+            }
+            //从上向下
+            for (int row = leftTopX + 1; row <= rightBottomX; row++){
+                result.add(array[row][rightBottomY]);
+            }
+            if(rightBottomX > leftTopX
+                    && rightBottomY > leftTopY){
+                //从右向左
+                for (int col = rightBottomY - 1; col > leftTopY; col--){
+                    result.add(array[rightBottomX][col]);
+                }
+                //从下向上
+                for (int row = rightBottomX; row > leftTopX; row--){
+                    result.add(array[row][leftTopY]);
+                }
+            }
+            leftTopX++;
+            leftTopY++;
+            rightBottomX--;
+            rightBottomY--;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 //        int[][] array = {{2,8,9},{7,9,12}};
 //        boolean res = Problem.problem4(array,8);
 //        System.out.println(res);
-
-        int[] array = {1,2,3,4,5};
-        problem6(array);
-//        problem5(array);
-        for (int a: array) {
+        int[][] array = {{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}};
+        List<Integer> list = problem7(array);
+        for (int a:list) {
             System.out.print(a + " ");
         }
     }
