f22b31f8f08cb695e6780725cb0be0feac885ac1
gaidongxu <gaidongxu@dataexa.com>
2021-03-15 12:56:10 +0800

    新的练习题

--- src/main/java/com/gai/interview/recursion/Problem.java
+++ src/main/java/com/gai/interview/recursion/Problem.java
@@ -293,6 +293,30 @@ public class Problem {
         return n;
     }
 
+    /*
+    *   问题11 构建乘积数组
+    *
+    *   给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1].
+    *   其中 B[i] 的值是数组 A 中除了下标 i 以外的元素的积.
+    *   即 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+    * */
+    public static int[] problem11(int[] a){
+        if(a == null || a.length == 0)
+            return new int[0];
+        int[] b = new int[a.length];
+        b[0] = 1;
+        int temp = 1;
+
+        for (int i = 1; i < a.length; i++){
+            b[i] = b[i - 1] * a[i - 1];
+        }
+        for (int i = a.length - 2; i >= 0; i--){
+            temp *= a[i + 1];
+            b[i] *= temp;
+        }
+        return b;
+    }
+
     public static void main(String[] args) {
         System.out.println(problem8(new int[]{0,0,1,2,5}));
     }
